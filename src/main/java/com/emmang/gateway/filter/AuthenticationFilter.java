971fae5d2afdd658c5915e71d8c4efa19babaafd
EmmaNguyen0410 <emmang0410@gmail.com>
2025-01-24 14:29:30 +1100

    first commit

---
+++ src/main/java/com/emmang/gateway/filter/AuthenticationFilter.java
@@ -0,0 +1,62 @@
+package com.emmang.gateway.filter;
+
+import com.emmang.gateway.util.JWTUtil;
+import io.jsonwebtoken.Claims;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+
+@Component
+public class AuthenticationFilter extends AbstractGatewayFilterFactory<AuthenticationFilter.Config> {
+
+    private final RouteValidator routeValidator;
+
+    private final JWTUtil jwtUtil;
+
+    public AuthenticationFilter(RouteValidator routeValidator, JWTUtil jwtUtil) {
+        super(Config.class);
+        this.routeValidator = routeValidator;
+        this.jwtUtil = jwtUtil;
+    }
+
+    @Override
+    public GatewayFilter apply(Config config) {
+        return ((exchange, chain) -> {
+            ServerHttpRequest request = exchange.getRequest();
+            if (routeValidator.isSecured.test(request)) {
+                String authHeader = request.getHeaders().getFirst("Authorization");
+                String token = "";
+
+                if (authHeader != null && authHeader.startsWith("Bearer ")) {
+                    token = authHeader.substring(7);
+                } else {
+                    throw new RuntimeException("Missing Authorization Header");
+                }
+
+                try {
+                    if (!jwtUtil.isTokenValid(token)) {
+                        throw new RuntimeException("Invalid JWT Token");
+                    }
+                    return chain.filter(populateRequestWithHeaders(exchange, jwtUtil.extractAllClaims(token)));
+                } catch (Exception e) {
+                    throw new RuntimeException("Invalid JWT Token");
+                }
+            }
+            return chain.filter(exchange);
+        });
+    }
+
+    private ServerWebExchange populateRequestWithHeaders(ServerWebExchange exchange, Claims claims) {
+        ServerHttpRequest mutatedRequest = exchange.getRequest().mutate()
+                .header("username",String.valueOf(claims.get("username")))
+                .header("role", String.valueOf(claims.get("role")))
+                .build();
+
+        return exchange.mutate().request(mutatedRequest).build();
+    }
+
+    public static class Config {}
+
+}
---
+++ src/main/java/com/emmang/gateway/util/JWTUtil.java
@@ -0,0 +1,48 @@
+package com.emmang.gateway.util;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.function.Function;
+
+@Component
+public class JWTUtil {
+    @Value("${security.jwt.secret-key}")
+    private String secretKey;
+
+    @Value("${security.jwt.expiration-time}")
+    private String expirationTime;
+
+    public Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
+    }
+
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    // We can only parse the token if we know the secret key that was used to create the JWT in the first place.
+    public Claims extractAllClaims(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(getSignInKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    private Key getSignInKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(secretKey);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    public boolean isTokenValid(String token) {
+        return !extractExpiration(token).before(new Date());
+    }
+}
